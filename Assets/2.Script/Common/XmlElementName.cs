3690b8b375fc1d5efb1eb290c8e6e12d2241df4c
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-20 00:45:11 +0900

    xml element의 이름 및 구조 수정

--- Assets/2.Script/Common/XmlElementName.cs
+++ Assets/2.Script/Common/XmlElementName.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class XmlElementName
 {
+    public const string SOUND = "Sound";
+    // Data Attribute
+    public const string LENGTH = "Length";
+
+    // Identity Element
+    public const string IDENTITY = "Identity";
+    // Common Identity Attribute
+    public const string ID = "ID";
+    public const string NAME = "Name";
+    public const string CLIPPATH = "ClipPath";
+    public const string CLIPNAME = "ClipName";
+
     public class SoundData
     {
-        public const string LENGTH = "Length";
-
-        // Identity
-        public const string ID = "ID";
-        public const string NAME = "Name";
+        // Options Element
+        public const string OPTIONS = "Options";
+        // Options Attribute
         public const string PLAYTYPE = "PlayType";
-        public const string CLIPNAME = "ClipName";
-        public const string CLIPPATH = "ClipPath";
-
-        // Options
         public const string MAXVOLUME = "MaxVolume";
         public const string PITCH = "Pitch";
         public const string SPATIALBLEND = "SpatialBlend";
 
-        // Loop
+        // Loop Element
+        public const string LOOPOPTIONS = "LoopOptions";
+        // Loop Attribute
         public const string ISLOOP = "Loop";
         public const string CNTLOOP = "CntLoop";
         public const string STARTLOOP = "StartLoop";
