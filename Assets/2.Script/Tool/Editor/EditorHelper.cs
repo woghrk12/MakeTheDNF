18b2909bc1a634045705b79b3f988a7cfa85449d
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-19 19:07:21 +0900

    에디터 툴 상단 버튼 및 목록 리스트 기능 추가

--- Assets/2.Script/Tool/Editor/EditorHelper.cs
+++ Assets/2.Script/Tool/Editor/EditorHelper.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEngine;
 using UnityEditor;
+
+using UnityObject = UnityEngine.Object;
 
 public class EditorHelper
 {
@@ -43,4 +43,63 @@ public class EditorHelper
     }
 
     #endregion Helper Methods
+
+    #region Editor Function
+
+    public static void EditorToolTopLayer(BaseData p_data, ref int p_selection, ref UnityObject p_clip, int p_uiWidth)
+    {
+        EditorGUILayout.BeginHorizontal();
+        {
+            if (GUILayout.Button("Add", GUILayout.Width(p_uiWidth)))
+            {
+                p_data.AddData("New Clip");
+                p_selection = p_data.DataCount - 1;
+                p_clip = null;
+            }
+            if (GUILayout.Button("Copy", GUILayout.Width(p_uiWidth)))
+            {
+                p_data.CopyData(p_selection);
+                p_selection = p_data.DataCount - 1;
+                p_clip = null;
+            }
+            if (GUILayout.Button("Remove", GUILayout.Width(p_uiWidth)))
+            {
+                p_clip = null; 
+                p_data.RemoveData(p_selection);
+            }
+            if (GUILayout.Button("Clear", GUILayout.Width(p_uiWidth)))
+            {
+                p_clip = null;
+                p_data.ClearData();
+            }
+            if (p_selection >= p_data.DataCount) p_selection = p_data.DataCount - 1;
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    public static void EditorToolListLayer(BaseData p_data, ref Vector2 p_scrollPos, ref int p_selection, ref UnityObject p_clip, int p_uiWidth)
+    {
+        EditorGUILayout.BeginVertical(GUILayout.Width(p_uiWidth));
+        {
+            EditorGUILayout.LabelField("Source List");
+            EditorGUILayout.Separator();
+            EditorGUILayout.BeginVertical("box");
+            {
+                p_scrollPos = EditorGUILayout.BeginScrollView(p_scrollPos);
+                {
+                    if (p_data.DataCount > 0)
+                    {
+                        int t_lastSelection = p_selection;
+                        p_selection = GUILayout.SelectionGrid(p_selection, p_data.GetNameList(true), 1);
+                        if (t_lastSelection != p_selection) p_clip = null;
+                    }
+                }
+                EditorGUILayout.EndScrollView();
+            }
+            EditorGUILayout.EndVertical();
+        }
+        EditorGUILayout.EndVertical();
+    }
+
+    #endregion Editor Function
 }
