c179fc9330be65764a1428780c47e25132f0f89b
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-23 17:55:43 +0900

    키 클래스 이름 변경

--- Assets/2.Script/Common/ButtonName.cs
+++
@@ -1,6 +0,0 @@
-public class ButtonName
-{
-    public const string Horizontal = "Horizontal";
-    public const string Vertical = "Vertical";
-    public const string Jump = "Jump"; 
-}
---
+++ Assets/2.Script/Common/KeyName.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class KeyName
+{
+    public static KeyCode PosHorizontal = KeyCode.RightArrow;
+    public static KeyCode NegHorizontal = KeyCode.LeftArrow;
+    public static KeyCode PosVertical = KeyCode.UpArrow;
+    public static KeyCode NegVertical = KeyCode.DownArrow;
+    public static KeyCode Attack = KeyCode.X;
+    public static KeyCode Jump = KeyCode.C;
+    public static KeyCode SkillSlot1 = KeyCode.A;
+    public static KeyCode SkillSlot2 = KeyCode.S;
+    public static KeyCode SkillSlot3 = KeyCode.D;
+    public static KeyCode SkillSlot4 = KeyCode.F;
+    public static KeyCode SkillSlot5 = KeyCode.G;
+    public static KeyCode SkillSlot6 = KeyCode.H;
+}
