326a9b7f4b378f642a4c5fb31999bf24f34906de
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-25 16:47:50 +0900

    애니메이션 키 클래스 구분

--- Assets/2.Script/Common/AnimatorKey.cs
+++ Assets/2.Script/Common/AnimatorKey.cs
@@ -5,10 +5,14 @@ public class AnimatorKey
     public const string JumpStay = "JumpStay";
     public const string JumpDown = "JumpDown";
     public const string IsAttack = "IsAttack";
-    public const string BaseAttack0 = "BaseAttack0";
-    public const string BaseAttack1 = "BaseAttack1";
-    public const string BaseAttack2 = "BaseAttack2";
-    public const string SoftSlash = "SoftSlash";
-    public const string UpperSlash = "UpperSlash";
-    public const string StrongSlash = "StrongSlash";
+
+    public class Warrior
+    {
+        public const string BaseAttack0 = "BaseAttack0";
+        public const string BaseAttack1 = "BaseAttack1";
+        public const string BaseAttack2 = "BaseAttack2";
+        public const string SoftSlash = "SoftSlash";
+        public const string UpperSlash = "UpperSlash";
+        public const string StrongSlash = "StrongSlash";
+    }
 }
