2cc50943dbb4dadb24664aa8db2acc6076054849
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-25 16:41:36 +0900

    캐릭터 공격 애니메이션 추가

--- Assets/2.Script/Common/AnimatorKey.cs
+++ Assets/2.Script/Common/AnimatorKey.cs
@@ -4,4 +4,11 @@ public class AnimatorKey
     public const string IsJump = "IsJump";
     public const string JumpStay = "JumpStay";
     public const string JumpDown = "JumpDown";
+    public const string IsAttack = "IsAttack";
+    public const string BaseAttack0 = "BaseAttack0";
+    public const string BaseAttack1 = "BaseAttack1";
+    public const string BaseAttack2 = "BaseAttack2";
+    public const string SoftSlash = "SoftSlash";
+    public const string UpperSlash = "UpperSlash";
+    public const string StrongSlash = "StrongSlash";
 }
