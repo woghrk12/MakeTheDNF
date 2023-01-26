5e6de8af3f449b9a9d3ccacded6c73d91b6f8b20
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-26 23:45:45 +0900

    Warrior 애니메이션 수정

--- Assets/2.Script/Common/AnimatorKey.cs
+++ Assets/2.Script/Common/AnimatorKey.cs
@@ -8,9 +8,6 @@ public class AnimatorKey
 
     public class Warrior
     {
-        public const string BaseAttack0 = "BaseAttack0";
-        public const string BaseAttack1 = "BaseAttack1";
-        public const string BaseAttack2 = "BaseAttack2";
         public const string SoftSlash = "SoftSlash";
         public const string UpperSlash = "UpperSlash";
         public const string StrongSlash = "StrongSlash";
