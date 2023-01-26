c7ce416137d5fef9add301aa8a2702957a4e7550
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-26 17:52:57 +0900

    Skill 사용 키 등록 및 제거 로직 수정

--- Assets/2.Script/Manager/SkillManager.cs
+++ Assets/2.Script/Manager/SkillManager.cs
@@ -6,27 +6,25 @@ public class SkillManager : SingletonMonobehaviour<SkillManager>
 {
     #region Variables
     
-    public Skill baseAttack;
-    private static Dictionary<KeyCode, Skill> skills = new Dictionary<KeyCode, Skill>();
+    private static Dictionary<PlayerKey, Skill> skills = new Dictionary<PlayerKey, Skill>();
 
     #endregion Variables
 
     #region Properties
 
-    public static Dictionary<KeyCode, Skill> Skills => skills;
+    public static Dictionary<PlayerKey, Skill> Skills => skills;
 
     #endregion Properties
 
     #region Methods
 
-    public static void RegisterSkill(KeyCode p_key, Skill p_newSkill)
+    public static void RegisterSkill(PlayerKey p_key, Skill p_newSkill)
     {
         if (skills.ContainsKey(p_key)) skills.Remove(p_key);
-
         skills.Add(p_key, p_newSkill);
     }
 
-    public static void RemoveSkill(KeyCode p_key)
+    public static void RemoveSkill(PlayerKey p_key)
     {
         if (skills.ContainsKey(p_key)) skills.Remove(p_key);
     }
