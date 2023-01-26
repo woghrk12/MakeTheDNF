284bcddde4d320582e80a734437a0c89f9bee7c0
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-26 22:05:21 +0900

    히트박스 에디터 기능 개선 (F1 키를 통해 히트박스 크기 조절 기능과 오프셋 조절 기능을 번갈아 사용할 수 있음)

--- Assets/2.Script/Editor/Hitbox_Editor.cs
+++ Assets/2.Script/Editor/Hitbox_Editor.cs
@@ -8,6 +8,7 @@ public class Hitbox_Editor : Editor
 {
     #region Variables
 
+    private bool isChangeOffset = false;
     private Hitbox hitbox = null;
 
     #endregion Variables
@@ -22,84 +23,67 @@ public class Hitbox_Editor : Editor
     {
         if (!hitbox || hitbox.hitboxType == Hitbox.EHitboxType.NONE) return;
 
+        Event t_curEvent = Event.current;
+        if (!Application.isPlaying && t_curEvent.type == EventType.KeyDown && t_curEvent.keyCode == KeyCode.F1) isChangeOffset = !isChangeOffset;
+
         var t_origin = new Vector3(hitbox.CharTransform.X, hitbox.CharTransform.Y + hitbox.CharTransform.Z * DNFTransform.convRate, 0f);
         var t_center = t_origin + new Vector3(hitbox.offset.x, hitbox.offset.y + hitbox.offset.z * DNFTransform.convRate, 0f);
 
-        // Set offset along X axis of DNF transform
-        Handles.color = Color.red;
-        t_center = Handles.Slider(t_center, hitbox.transform.right);
-        hitbox.offset.x = t_center.x - t_origin.x;
+        if (isChangeOffset)
+        {
+            // Set offset along X axis of DNF transform
+            Handles.color = Color.red;
+            t_center = Handles.Slider(t_center, hitbox.transform.right);
+            hitbox.offset.x = t_center.x - t_origin.x;
 
-        // Set offset along Z axis of DNF transform
-        Handles.color = Color.blue;
-        t_center = Handles.Slider(t_center, hitbox.transform.up * (-0.1f));
-        hitbox.offset.z = (t_center.y - t_origin.y - hitbox.offset.y) * DNFTransform.invConvRate;
+            // Set offset along Z axis of DNF transform
+            Handles.color = Color.blue;
+            t_center = Handles.Slider(t_center, hitbox.transform.up * (-0.1f));
+            hitbox.offset.z = (t_center.y - t_origin.y - hitbox.offset.y) * DNFTransform.invConvRate;
 
-        // Set offset along Y axis of DNF transform
-        Handles.color = Color.green;
-        t_center = Handles.Slider(t_center, hitbox.transform.up);
-        hitbox.offset.y = t_center.y - t_origin.y - hitbox.offset.z * DNFTransform.convRate;
-
-        if (hitbox.hitboxType == Hitbox.EHitboxType.BOX)
+            // Set offset along Y axis of DNF transform
+            Handles.color = Color.green;
+            t_center = Handles.Slider(t_center, hitbox.transform.up);
+            hitbox.offset.y = t_center.y - t_origin.y - hitbox.offset.z * DNFTransform.convRate;
+        }
+        else
         {
             // Set size along x axis of DNF transform
             Handles.color = Color.red;
-            hitbox.size.x = Handles.ScaleSlider(
-                hitbox.size.x, 
-                t_center + new Vector3(hitbox.size.x * 0.5f, -hitbox.size.z * 0.5f * DNFTransform.convRate, 0f), 
-                hitbox.transform.right, 
-                Quaternion.identity, 
-                1f, 
-                0.1f);
+            hitbox.size.x = Handles.ScaleSlider(hitbox.size.x, t_center, hitbox.transform.right, Quaternion.identity, 1f, 0.1f);
 
-            // Set size along z axis of DNF transform
-            Handles.color = Color.blue;
-            hitbox.size.z = Handles.ScaleSlider(
-                hitbox.size.z, 
-                t_center + new Vector3(-hitbox.size.x * 0.5f, -hitbox.size.z * 0.5f * DNFTransform.convRate, 0f), 
-                hitbox.transform.up * (-1.0f), 
-                Quaternion.identity, 
-                1f, 
-                0.1f);
-            
+            if (hitbox.hitboxType == Hitbox.EHitboxType.BOX)
+            {
+                // Set size along z axis of DNF transform
+                Handles.color = Color.blue;
+                hitbox.size.z = Handles.ScaleSlider(hitbox.size.z, t_center, hitbox.transform.up * (-1.0f), Quaternion.identity, 1f, 0.1f);
+            }
+
+            // Set size along y axis of DNF transform
+            Handles.color = Color.green;
+            hitbox.size.y = Handles.ScaleSlider(hitbox.size.y, t_center, hitbox.transform.up, Quaternion.identity, 1f, 0.1f);
+        }
+
+        Handles.color = Color.red;
+        if (hitbox.hitboxType == Hitbox.EHitboxType.BOX)
+        {
             // Show box hitbox range of x, z axis of DNF transform
-            Handles.color = Color.red;
             Handles.DrawWireCube(t_center - new Vector3(0f, hitbox.offset.y, 0f), new Vector3(hitbox.size.x, hitbox.size.z * DNFTransform.convRate, 0f));
         }
         else if (hitbox.hitboxType == Hitbox.EHitboxType.CIRCLE)
         {
-            // Set radius of circle 
-            Handles.color = Color.red;   
-            hitbox.size.x = Handles.ScaleSlider(
-                hitbox.size.x,
-                t_center + new Vector3(hitbox.size.x * 0.5f, 0f, 0f),
-                hitbox.transform.right, 
-                Quaternion.identity,
-                1f, 
-                0.1f);
-
             // Show circle hitbox range of x, z axis of DNF transform
             float t_angle = Mathf.Acos(DNFTransform.convRate) * Mathf.Rad2Deg;
-            Handles.color = Color.red;
             Handles.DrawWireArc(
-                t_center, 
+                t_center,
                 Quaternion.AngleAxis(t_angle, hitbox.transform.right) * hitbox.transform.forward,
-                hitbox.transform.right, 
-                360, 
+                hitbox.transform.right,
+                360,
                 hitbox.size.x * 0.5f);
         }
 
-        // Set size along y axis of DNF transform
-        Handles.color = Color.green;
-        hitbox.size.y = Handles.ScaleSlider(
-            hitbox.size.y,
-            t_center + new Vector3(-hitbox.size.x * 0.5f, hitbox.size.y, 0f),
-            hitbox.transform.up, 
-            Quaternion.identity, 
-            1f, 
-            0.1f);
-
         // Show box hitbox range of x, y axis of DNF transform
+        Handles.color = Color.green;
         Handles.DrawWireCube(
             t_center - new Vector3(0f, hitbox.offset.z * DNFTransform.convRate, 0f) + new Vector3(0f, hitbox.size.y * 0.5f, 0f),
             new Vector3(hitbox.size.x, hitbox.size.y, 0f));
