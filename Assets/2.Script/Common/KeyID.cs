9db688620ede55a73180405579821ab86272fb13
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-30 17:04:49 +0900

    Key 정보 클래스 수정

---
+++ Assets/2.Script/Common/KeyID.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class KeyID
+{
+    public static int PosHorizontal = 0;
+    public static int NegHorizontal = 1;
+    public static int PosVertical = 2;
+    public static int NegVertical = 3;
+    public static int Attack = 4;
+    public static int Jump = 5;
+    public static int SkillSlot1 = 6;
+    public static int SkillSlot2 = 7;
+    public static int SkillSlot3 = 8;
+    public static int SkillSlot4 = 9;
+    public static int SkillSlot5 = 10;
+    public static int SkillSlot6 = 11;
+    public static int Inventory = 12;
+    public static int SkillTree = 13;
+    public static int CharInfo = 14;
+    public static int End = 15;
+}
