8043b695ff1b88cc6f796e342b3f104ec35fef12
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-02-01 01:00:08 +0900

    Sprite 폴더 경로 변수 추가

--- Assets/2.Script/Common/FilePath.cs
+++ Assets/2.Script/Common/FilePath.cs
@@ -3,4 +3,5 @@ public class FilePath
     public const string EnumFolderPath = "Assets/2.Script/GameData/";
     public const string EnumTemplateFilePath = "Assets/Editor/EnumTemplate.txt";
     public const string DataDirectoryPath = "/9.Resources/Resources/Data/";
+    public const string SpriteDirectoryPath = "/9.Resources/Resources/Sprite/SkillEffect/";
 }
