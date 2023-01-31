ee5628ac2548d38537bd77806068089b1713ed5e
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-31 19:58:56 +0900

    Input Data XML 이름 변수 추가

--- Assets/2.Script/Common/XmlElementName.cs
+++ Assets/2.Script/Common/XmlElementName.cs
@@ -6,6 +6,7 @@ public class XmlElementName
 {
     public const string SOUND = "Sound";
     public const string EFFECT = "Effect";
+    public const string INPUT = "Input";
     // Data Attribute
     public const string LENGTH = "Length";
 
@@ -40,5 +41,40 @@ public class XmlElementName
     public class EffectData
     { 
     
+    }
+
+    public class InputData
+    {
+        // Control movement behaviour
+        public const string MOVE = "Move";
+        public const string POSHORIZONTAL = "PosHorizontal";
+        public const string NEGHORIZONTAL = "NegHorizontal";
+        public const string POSVERTICAL = "PosVertical";
+        public const string NEGVERTICAL = "NegVertical";
+
+        // Control basic behaviour
+        public const string Attack = "Attack";
+        public const string Jump = "Jump";
+
+        // Control skill slots
+        public const string SKILLSLOTS = "SkillSlots";
+        public const string SKILLSLOT1 = "SkillSlot1";
+        public const string SKILLSLOT2 = "SkillSlot2";
+        public const string SKILLSLOT3 = "SkillSlot3";
+        public const string SKILLSLOT4 = "SkillSlot4";
+        public const string SKILLSLOT5 = "SkillSlot5";
+        public const string SKILLSLOT6 = "SkillSlot6";
+        public const string SKILLSLOT7 = "SkillSlot7";
+        public const string SKILLSLOT8 = "SkillSlot8";
+        public const string SKILLSLOT9 = "SkillSlot9";
+        public const string SKILLSLOT10 = "SkillSlot10";
+        public const string SKILLSLOT11 = "SkillSlot11";
+        public const string SKILLSLOT12 = "SkillSlot12";
+
+        // Control UI
+        public const string UI = "UI";
+        public const string INVENTORY = "Inventory";
+        public const string SKILL = "Skill";
+        public const string CHARINFO = "CharInfo";
     }
 }
