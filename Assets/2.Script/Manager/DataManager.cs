3e512e9bde25e4e7dec237b52298f3b565490d6a
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-31 19:59:41 +0900

    Data Manager를 통해 Input Data 반환 로직 추가

--- Assets/2.Script/Manager/DataManager.cs
+++ Assets/2.Script/Manager/DataManager.cs
@@ -6,12 +6,27 @@ public class DataManager : SingletonMonobehaviour<DataManager>
 {
     #region Variables
 
+    private static InputData inputData = null;
     private static SoundData soundData = null;
     private static EffectData effectData = null;
 
     #endregion Variables
 
     #region Properties
+
+    public static InputData InputData
+    {
+        get
+        {
+            if (ReferenceEquals(inputData, null))
+            {
+                inputData = ScriptableObject.CreateInstance<InputData>();
+                inputData.LoadData();
+            }
+
+            return inputData;
+        }
+    }
 
     public static SoundData SoundData
     {
@@ -40,6 +55,7 @@ public class DataManager : SingletonMonobehaviour<DataManager>
             return effectData;
         }
     }
+
     #endregion Properties
 
     private void Start()
