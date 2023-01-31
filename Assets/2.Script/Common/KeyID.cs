c4f4f61a0689f1518b17b4014d0b08ad9a1c90cc
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-01-31 17:38:59 +0900

    Key ID 클래스 수정

--- Assets/2.Script/Common/KeyID.cs
+++ Assets/2.Script/Common/KeyID.cs
@@ -1,21 +1,38 @@
 using UnityEngine;
 
 public class KeyID
-{
-    public static int PosHorizontal = 0;
-    public static int NegHorizontal = 1;
-    public static int PosVertical = 2;
-    public static int NegVertical = 3;
-    public static int BaseAttack = 4;
-    public static int Jump = 5;
-    public static int SkillSlot1 = 6;
-    public static int SkillSlot2 = 7;
-    public static int SkillSlot3 = 8;
-    public static int SkillSlot4 = 9;
-    public static int SkillSlot5 = 10;
-    public static int SkillSlot6 = 11;
-    public static int Inventory = 12;
-    public static int SkillTree = 13;
-    public static int CharInfo = 14;
-    public static int End = 15;
+{ 
+    public class Move
+    {
+        public static int PosHorizontal = 0;
+        public static int NegHorizontal = 1;
+        public static int PosVertical = 2;
+        public static int NegVertical = 3;
+        public static int End = 4;
+    }
+
+    public class SkillSlot
+    {
+        public static int SkillSlot1 = 0;
+        public static int SkillSlot2 = 1;
+        public static int SkillSlot3 = 2;
+        public static int SkillSlot4 = 3;
+        public static int SkillSlot5 = 4;
+        public static int SkillSlot6 = 5;
+        public static int SkillSlot7 = 6;
+        public static int SkillSlot8 = 7;
+        public static int SkillSlot9 = 8;
+        public static int SkillSlot10 = 9;
+        public static int SkillSlot11 = 10;
+        public static int SkillSlot12 = 11;
+        public static int End = 12;
+    }
+
+    public class UI
+    {
+        public static int Inventory = 12;
+        public static int SkillTree = 13;
+        public static int CharInfo = 14;
+
+    }
 }
