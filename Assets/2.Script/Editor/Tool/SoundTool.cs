ad80e3ce73b0fd969ce6d20d1c64a5d068f163a5
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-02-12 18:39:54 +0900

    사운드 툴 기능 보완

--- Assets/2.Script/Editor/Tool/SoundTool.cs
+++ Assets/2.Script/Editor/Tool/SoundTool.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 using UnityObject = UnityEngine.Object;
@@ -14,6 +16,10 @@ public class SoundTool : EditorWindow
     private Vector2 scrollPos2 = Vector2.zero;
 
     private static SoundData soundData;
+    private const string enumName = "SoundList";
+
+    private bool isSelectedAnother = false;
+    private ReorderableList loopList;
 
     #endregion Variables
 
@@ -36,29 +42,16 @@ public class SoundTool : EditorWindow
             
             EditorGUILayout.BeginHorizontal();
             {
+                int t_oldSelection = selection;
                 EditorToolLayer<SoundClip>.EditorToolListLayer(soundData, ref scrollPos1, ref selection, EditorHelper.uiWidthMiddle);
-                if(selection >= 0) InfoLayer();
-            }
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.Separator();
-
-            EditorGUILayout.BeginHorizontal();
-            {
-                if (GUILayout.Button("Reload Settings"))
+                if (selection >= 0)
                 {
-                    soundData = CreateInstance<SoundData>();
-                    soundData.LoadData();
-                    selection = 0;
-                }
-                if (GUILayout.Button("Save Settings"))
-                {
-                    soundData.SaveData();
-                    CreateEnumStructure();
-                    AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+                    isSelectedAnother = t_oldSelection != selection;
+                    InfoLayer();
                 }
             }
             EditorGUILayout.EndHorizontal();
+            EditorToolLayer<SoundClip>.EditorToolBottomLayer(soundData, ref selection, enumName);
         }
         EditorGUILayout.EndVertical();
     }
@@ -67,8 +60,6 @@ public class SoundTool : EditorWindow
 
     private void InfoLayer()
     {
-        if (soundData.DataCount <= 0) return;
-
         EditorGUILayout.BeginVertical();
         {
             EditorGUILayout.LabelField("Setting", EditorStyles.boldLabel);
@@ -77,46 +68,74 @@ public class SoundTool : EditorWindow
             {
                 SoundClip t_clip = soundData.soundClips[selection];
 
+                if (isSelectedAnother)
+                {
+                    // Initialize Loop List
+                    loopList = new ReorderableList(ArrayHelper.ArrayToList(t_clip.checkTime), typeof(int));
+                    loopList.drawElementCallback = (Rect p_rect, int p_idx, bool p_isActive, bool p_isFocused) =>
+                    {
+                        p_rect.height = EditorGUIUtility.singleLineHeight;
+
+                        p_rect.width = 50f;
+                        EditorGUI.LabelField(p_rect, p_idx.ToString());
+                        p_rect.x += p_rect.width;
+
+                        p_rect.width = 100f;
+                        EditorGUI.LabelField(p_rect, "Check Time");
+                        p_rect.x += p_rect.width;
+
+                        p_rect.width = EditorHelper.uiWidthSmall;
+                        t_clip.checkTime[p_idx] = EditorGUI.FloatField(p_rect, t_clip.checkTime[p_idx]);
+                        p_rect.x += p_rect.width + 50f;
+
+                        p_rect.width = 100f;
+                        EditorGUI.LabelField(p_rect, "Set Time");
+                        p_rect.x += p_rect.width;
+
+                        p_rect.width = EditorHelper.uiWidthSmall;
+                        t_clip.setTime[p_idx] = EditorGUI.FloatField(p_rect, t_clip.setTime[p_idx]);
+                    };
+                    loopList.drawHeaderCallback = (Rect p_rect) => { EditorGUI.LabelField(p_rect, "Loop List"); };
+                    loopList.onAddCallback = (ReorderableList p_list) => 
+                    {
+                        int t_idx = p_list.index;
+                        t_clip.AddLoop();
+                        p_list.list.Add(0.0f);
+                        p_list.index = t_idx;
+                    };
+                    loopList.onRemoveCallback = (ReorderableList p_list) => 
+                    {
+                        int t_idx = p_list.index;
+                        p_list.list.RemoveAt(t_idx);
+                        t_clip.RemoveLoop(t_idx);
+                        p_list.index = 0;
+                    };
+                }
+
+                // Identity
                 EditorGUILayout.LabelField("ID", t_clip.clipID.ToString(), GUILayout.Width(EditorHelper.uiWidthLarge));
                 soundData.names[selection] = EditorGUILayout.TextField("Name", soundData.names[selection], GUILayout.Width(EditorHelper.uiWidthLarge));
 
-                if (t_clip.Clip == null && t_clip.clipName != string.Empty) t_clip.PreLoad();
+                EditorGUILayout.Separator();
 
+                // Audio Clip
+                EditorGUILayout.LabelField("Audio Clip", EditorStyles.boldLabel);
+                if (t_clip.Clip == null && t_clip.clipName != string.Empty) t_clip.PreLoad();
                 t_clip.Clip = EditorGUILayout.ObjectField("Audio Clip", t_clip.Clip, typeof(AudioClip), false, GUILayout.Width(EditorHelper.uiWidthLarge)) as AudioClip;
 
                 if (t_clip.Clip != null)
                 {
                     t_clip.clipPath = EditorHelper.GetPath(t_clip.Clip);
                     t_clip.clipName = t_clip.Clip.name;
-
-                    EditorGUILayout.Separator();
-                    EditorGUILayout.LabelField("Options");
                     t_clip.maxVolume = EditorGUILayout.FloatField("Max Volume", t_clip.maxVolume, GUILayout.Width(EditorHelper.uiWidthLarge));
                     t_clip.pitch = EditorGUILayout.Slider("Pitch", t_clip.pitch, -3.0f, 3.0f, GUILayout.Width(EditorHelper.uiWidthLarge));
                     t_clip.spatialBlend = EditorGUILayout.Slider("Pan Level", t_clip.spatialBlend, 0.0f, 1.0f, GUILayout.Width(EditorHelper.uiWidthLarge));
 
                     EditorGUILayout.Separator();
+
+                    // Loop Option
                     t_clip.isLoop = EditorGUILayout.Toggle("Loop", t_clip.isLoop, GUILayout.Width(EditorHelper.uiWidthLarge));
-                    if (t_clip.isLoop)
-                    {
-                        if (GUILayout.Button("Add Loop", GUILayout.Width(EditorHelper.uiWidthMiddle))) t_clip.AddLoop();
-                        for (int i = 0; i < t_clip.cntLoop; i++)
-                        {
-                            EditorGUILayout.BeginVertical();
-                            {
-                                EditorGUILayout.LabelField("Loop Step " + i, EditorStyles.boldLabel);
-                                if (GUILayout.Button("Remove", GUILayout.Width(EditorHelper.uiWidthMiddle)))
-                                {
-                                    t_clip.RemoveLoop(i);
-                                    EditorGUILayout.EndVertical();
-                                    continue;
-                                }
-                                t_clip.setTime[i] = EditorGUILayout.FloatField("Set Time", t_clip.setTime[i], GUILayout.Width(EditorHelper.uiWidthMiddle));
-                                t_clip.checkTime[i] = EditorGUILayout.FloatField("Check Time", t_clip.checkTime[i], GUILayout.Width(EditorHelper.uiWidthMiddle));
-                            }
-                            EditorGUILayout.EndVertical();
-                        }
-                    }
+                    if (t_clip.isLoop) loopList.DoLayoutList();
                 }
                 else
                 {
@@ -132,25 +151,4 @@ public class SoundTool : EditorWindow
     }
 
     #endregion Panel Methods
-
-    #region Helper Methods
-
-    private void CreateEnumStructure()
-    {
-        StringBuilder t_builder = new StringBuilder();
-        t_builder.AppendLine();
-
-        int t_lenght = soundData.names != null ? soundData.DataCount : 0;
-        for (int i = 0; i < t_lenght; i++)
-        {
-            if (soundData.names[i] == string.Empty) continue;
-
-            string t_name = soundData.names[i];
-            t_name = string.Concat(t_name.Where(t_char => !char.IsWhiteSpace(t_char)));
-            t_builder.AppendLine("    " + t_name + " = " + i + ",");
-        }
-        EditorHelper.CreateEnumStructure("SoundList", t_builder);
-    }
-
-    #endregion Helper Methods
 }
