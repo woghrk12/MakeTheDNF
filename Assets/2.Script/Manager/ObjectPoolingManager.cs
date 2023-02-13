7084e8af046e9ca34f9d78212716be52d9422b7a
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-02-13 17:24:39 +0900

    오브젝트 풀링 기능 보완

--- Assets/2.Script/Manager/ObjectPoolingManager.cs
+++ Assets/2.Script/Manager/ObjectPoolingManager.cs
@@ -3,50 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public struct Pool
-{
-    public string tag;
-    public GameObject prefab;
-    public int size;
-}
-
 public class ObjectPoolingManager : SingletonMonobehaviour<ObjectPoolingManager>
 {
     #region Variables
 
-    [SerializeField] private Pool[] pools;
+    private Dictionary<string, GameObject> prefabs = new Dictionary<string, GameObject>();
+    private Dictionary<string, Transform> categories = new Dictionary<string, Transform>();
     private Dictionary<string, Queue<GameObject>> poolDictionary = new Dictionary<string, Queue<GameObject>>();
-    private Dictionary<string, GameObject> categories = new Dictionary<string, GameObject>();
 
     #endregion Variables
 
-    #region Unity Event
-
-    protected override void Awake()
-    {
-        base.Awake();
-
-        foreach (Pool t_pool in pools)
-        {
-            GameObject t_cateory = new GameObject(t_pool.tag);
-            t_cateory.transform.SetParent(transform);
-            categories.Add(t_pool.tag, t_cateory);
-
-            poolDictionary.Add(t_pool.tag, new Queue<GameObject>());
-
-            for (int i = 0; i < t_pool.size; i++) CreateNewObject(t_pool.tag, t_pool.prefab);
-        }
-    }
-
-    #endregion Unity Event
-
     #region Methods
 
-    public static GameObject Instantiate(string p_tag) => Instance.SpawnFromPool(p_tag, Vector3.zero, Quaternion.identity);
-    public static GameObject Instantiate(string p_tag, Vector3 p_pos) => Instance.SpawnFromPool(p_tag, p_pos, Quaternion.identity);
-    public static GameObject Instantiate(string p_tag, Vector3 p_pos, Quaternion p_rotation) => Instance.SpawnFromPool(p_tag, p_pos, p_rotation);
-    public static void Destroy(GameObject p_obj) => Instance.ReturnToPool(p_obj);
+    public void AddPool(GameObject p_obj, int p_size)
+    {
+        string t_tagName = p_obj.name;
+        Transform t_category = new GameObject(t_tagName).transform;
+        t_category.SetParent(transform);
+
+        prefabs.Add(t_tagName, p_obj);
+        categories.Add(t_tagName, t_category);
+        poolDictionary.Add(t_tagName, new Queue<GameObject>());
+
+        for (int i = 0; i < p_size; i++) CreateNewObject(t_tagName, p_obj);
+    }
+
+    public GameObject Instantiate(string p_tag) => SpawnFromPool(p_tag, Vector3.zero, Quaternion.identity);
+    public GameObject Instantiate(string p_tag, Vector3 p_pos) => SpawnFromPool(p_tag, p_pos, Quaternion.identity);
+    public GameObject Instantiate(string p_tag, Vector3 p_pos, Quaternion p_rotation) => SpawnFromPool(p_tag, p_pos, p_rotation);
+    public void Destroy(GameObject p_obj) => ReturnToPool(p_obj);
 
     #endregion Methods
 
@@ -54,13 +39,8 @@ public class ObjectPoolingManager : SingletonMonobehaviour<ObjectPoolingManager>
 
     private GameObject SpawnFromPool(string p_tag, Vector3 p_position, Quaternion p_rotation)
     {
-        Queue<GameObject> t_poolQueue = poolDictionary[p_tag];
-
-        if (t_poolQueue.Count <= 0)
-        {
-            Pool t_pool = Array.Find(pools, x => x.tag == p_tag);
-            CreateNewObject(t_pool.tag, t_pool.prefab);
-        }
+        if (!poolDictionary.TryGetValue(p_tag, out Queue<GameObject> t_poolQueue)) return null;
+        if (t_poolQueue.Count <= 0) CreateNewObject(p_tag, prefabs[p_tag]);
 
         GameObject t_obj = t_poolQueue.Dequeue();
 
@@ -81,7 +61,7 @@ public class ObjectPoolingManager : SingletonMonobehaviour<ObjectPoolingManager>
         GameObject t_obj = Instantiate(p_prefab, transform);
 
         t_obj.name = p_tag;
-        t_obj.transform.SetParent(categories[p_tag].transform);
+        t_obj.transform.SetParent(categories[p_tag]);
         t_obj.SetActive(false); 
 
         poolDictionary[p_tag].Enqueue(t_obj);
