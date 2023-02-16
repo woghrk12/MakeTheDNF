01d691c321c7185ae1394abec43b155643b6e118
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-02-16 22:33:10 +0900

    바뀐 데이터 관리 방식에 따른 SoundData 클래스 수정

--- Assets/2.Script/GameData/Sound/SoundData.cs
+++ Assets/2.Script/GameData/Sound/SoundData.cs
@@ -7,213 +7,32 @@ using UnityEngine;
 
 public class SoundData : BaseData<SoundClip>
 {
-    #region Variables
-
-    public SoundClip[] soundClips = new SoundClip[0];
-
-    private string dataPath = "Data/soundData";
-    private string xmlFileName = "soundData.xml";
-
-    #endregion Variables
-
     #region Override Methods
 
-    public override void LoadData()
+    public override string[] GetNameList(bool p_isShowID, string p_filterWord = "")
     {
-        xmlFilePath = Application.dataPath + FilePath.DataDirectoryPath;
-        TextAsset t_asset = Resources.Load(dataPath) as TextAsset;
-        if (t_asset == null || t_asset.text == null)
+        string[] t_retList = new string[0];
+        if (database == null) return t_retList;
+
+        t_retList = new string[DataCount];
+
+        for (int i = 0; i < DataCount; i++)
         {
-            AddData("New Clip");
-            return;
+            if (p_filterWord != "" && !database[i].clipName.ToLower().Contains(p_filterWord.ToLower())) continue;
+            t_retList[i] = p_isShowID ? i.ToString() + " : " + database[i].clipName : database[i].clipName;
         }
 
-        using (XmlReader t_reader = XmlReader.Create(new StringReader(t_asset.text)))
-        {
-            int t_length = 0;
-            int t_curID = 0;
-            int t_cntLoop = 0;
-
-            while (t_reader.Read())
-            {
-                if (t_reader.IsStartElement(XmlElementName.SOUND))
-                {
-                    t_length = int.Parse(t_reader.GetAttribute(XmlElementName.LENGTH));
-                    names = new string[t_length];
-                    soundClips = new SoundClip[t_length];
-                }
-
-                // Sound Identity
-                if (t_reader.IsStartElement(XmlElementName.SoundData.SOUNDSETTINGS))
-                {
-                    t_curID = int.Parse(t_reader.GetAttribute(XmlElementName.ID));
-                    names[t_curID] = t_reader.GetAttribute(XmlElementName.NAME);
-                    soundClips[t_curID] = new SoundClip();
-                }
-
-                // Sound Clip
-                if (t_reader.IsStartElement(XmlElementName.SoundData.CLIPPATH))
-                    soundClips[t_curID].clipPath = t_reader.ReadElementContentAsString();
-                if (t_reader.IsStartElement(XmlElementName.SoundData.CLIPNAME))
-                    soundClips[t_curID].clipName = t_reader.ReadElementContentAsString();
-
-                // Options
-                if (t_reader.IsStartElement(XmlElementName.SoundData.PLAYTYPE))
-                    soundClips[t_curID].playType = (SoundClip.ESoundPlayType)Enum.Parse(typeof(SoundClip.ESoundPlayType), t_reader.ReadElementContentAsString());
-                if (t_reader.IsStartElement(XmlElementName.SoundData.MAXVOLUME))
-                    soundClips[t_curID].maxVolume = float.Parse(t_reader.ReadElementContentAsString());
-                if (t_reader.IsStartElement(XmlElementName.SoundData.PITCH))
-                    soundClips[t_curID].pitch = float.Parse(t_reader.ReadElementContentAsString());
-                if (t_reader.IsStartElement(XmlElementName.SoundData.SPATIALBLEND))
-                    soundClips[t_curID].spatialBlend = float.Parse(t_reader.ReadElementContentAsString());
-
-                // Loop Options
-                if (t_reader.IsStartElement(XmlElementName.SoundData.ISLOOP))
-                    soundClips[t_curID].isLoop = bool.Parse(t_reader.ReadElementContentAsString());
-                if (t_reader.IsStartElement(XmlElementName.SoundData.CNTLOOP))
-                {
-                    t_cntLoop = int.Parse(t_reader.ReadElementContentAsString());
-                    soundClips[t_curID].cntLoop = t_cntLoop;
-                    soundClips[t_curID].checkTime = new float[t_cntLoop];
-                    soundClips[t_curID].setTime = new float[t_cntLoop];
-                }
-                if (t_reader.IsStartElement(XmlElementName.SoundData.STARTLOOP))
-                    soundClips[t_curID].startLoop = int.Parse(t_reader.ReadElementContentAsString());
-                if (t_reader.IsStartElement(XmlElementName.SoundData.CHECKTIME))
-                {
-                    string[] t_time = t_reader.ReadElementContentAsString().Split('/');
-                    for (int i = 0; i < t_cntLoop; i++)
-                    {
-                        if (t_time[i] == string.Empty) continue;
-                        soundClips[t_curID].checkTime[i] = float.Parse(t_time[i]);
-                    }
-                }
-                if (t_reader.IsStartElement(XmlElementName.SoundData.SETTIME))
-                {
-                    string[] t_time = t_reader.ReadElementContentAsString().Split('/');
-                    for (int i = 0; i < t_cntLoop; i++)
-                    {
-                        if (t_time[i] == string.Empty) continue;
-                        soundClips[t_curID].setTime[i] = float.Parse(t_time[i]);
-                    }
-                }
-            }
-        }
-    }
-
-    public override void SaveData()
-    {
-        XmlWriterSettings t_settings = new XmlWriterSettings();
-        t_settings.Encoding = System.Text.Encoding.Unicode;
-
-        using (XmlWriter t_writer = XmlWriter.Create(xmlFilePath + xmlFileName, t_settings))
-        {
-            t_writer.WriteStartDocument();
-            {
-                int t_length = DataCount;
-                t_writer.WriteStartElement(XmlElementName.SOUND);
-                t_writer.WriteAttributeString(XmlElementName.LENGTH, t_length.ToString());
-                {
-                    for (int i = 0; i < t_length; i++)
-                    {
-                        SoundClip t_clip = soundClips[i];
-
-                        t_writer.WriteStartElement(XmlElementName.SoundData.SOUNDSETTINGS);
-                        t_writer.WriteAttributeString(XmlElementName.ID, i.ToString());
-                        t_writer.WriteAttributeString(XmlElementName.NAME, names[i]);
-                        {
-                            t_writer.WriteStartElement(XmlElementName.SoundData.SOUNDCLIP);
-                            {
-                                t_writer.WriteElementString(XmlElementName.SoundData.CLIPPATH, t_clip.clipPath);
-                                t_writer.WriteElementString(XmlElementName.SoundData.CLIPNAME, t_clip.clipName);
-                            }
-                            t_writer.WriteEndElement();
-
-                            t_writer.WriteStartElement(XmlElementName.SoundData.OPTIONS);
-                            {
-                                t_writer.WriteElementString(XmlElementName.SoundData.PLAYTYPE, t_clip.playType.ToString());
-                                t_writer.WriteElementString(XmlElementName.SoundData.MAXVOLUME, t_clip.maxVolume.ToString());
-                                t_writer.WriteElementString(XmlElementName.SoundData.PITCH, t_clip.pitch.ToString());
-                                t_writer.WriteElementString(XmlElementName.SoundData.SPATIALBLEND, t_clip.spatialBlend.ToString());
-                            }
-                            t_writer.WriteEndElement();
-
-                            if (t_clip.isLoop)
-                            {
-                                t_writer.WriteStartElement(XmlElementName.SoundData.LOOPOPTIONS);
-                                {
-                                    t_writer.WriteElementString(XmlElementName.SoundData.ISLOOP, t_clip.isLoop.ToString());
-                                    t_writer.WriteElementString(XmlElementName.SoundData.CNTLOOP, t_clip.cntLoop.ToString());
-                                    t_writer.WriteElementString(XmlElementName.SoundData.STARTLOOP, t_clip.startLoop.ToString());
-
-                                    string t_str = "";
-                                    foreach (float t_checkTime in t_clip.checkTime) t_str += t_checkTime.ToString() + "/";
-                                    t_writer.WriteElementString(XmlElementName.SoundData.CHECKTIME, t_str);
-
-                                    t_str = "";
-                                    foreach (float t_setTime in t_clip.setTime) t_str += t_setTime.ToString() + "/";
-                                    t_writer.WriteElementString(XmlElementName.SoundData.SETTIME, t_str);
-                                }
-                                t_writer.WriteEndElement();
-                            }
-                        }
-                        t_writer.WriteEndElement();
-                    }
-                }
-                t_writer.WriteEndElement();
-            }
-            t_writer.WriteEndDocument();
-        }
-    }
-
-    public override SoundClip GetCopyClip(int p_idx)
-    {
-        if (p_idx < 0 || p_idx >= DataCount) return default;
-
-        SoundClip t_origin = soundClips[p_idx];
-        SoundClip t_copyClip = new SoundClip();
-
-        t_copyClip.clipID = t_origin.clipID;
-        t_copyClip.clipName = t_origin.clipName;
-        t_copyClip.clipPath = t_origin.clipPath;
-        t_copyClip.Clip = t_origin.Clip;
-        t_copyClip.playType = t_origin.playType;
-        t_copyClip.maxVolume = t_origin.maxVolume;
-        t_copyClip.pitch = t_origin.pitch;
-        t_copyClip.spatialBlend = t_origin.spatialBlend;
-
-        if (t_origin.isLoop)
-        {
-            int t_loopLength = t_origin.cntLoop;
-
-            t_copyClip.isLoop = t_origin.isLoop;
-            t_copyClip.cntLoop = t_loopLength;
-            t_copyClip.startLoop = t_origin.startLoop;
-
-            t_copyClip.checkTime = new float[t_loopLength];
-            t_copyClip.setTime = new float[t_loopLength];
-
-            for (int i = 0; i < t_loopLength; i++)
-            {
-                t_copyClip.checkTime[i] = t_origin.checkTime[i];
-                t_copyClip.setTime[i] = t_origin.setTime[i];
-            }
-        }
-
-        return t_copyClip;
+        return t_retList;
     }
 
     public override void AddData(string p_newName)
     {
-        if (names == null)
+        if (database == null)
         {
-            names = new string[] { p_newName };
-            soundClips = new SoundClip[] { new SoundClip() };
+            database = new SoundClip[] { new SoundClip() };
             return;
         }
-
-        names = ArrayHelper.Add(p_newName, names);
-        soundClips = ArrayHelper.Add(new SoundClip(), soundClips);
+        database = ArrayHelper.Add(new SoundClip(), database);
     }
 
     public override void RemoveData(int p_idx)
@@ -221,30 +40,22 @@ public class SoundData : BaseData<SoundClip>
         if (p_idx < 0 || p_idx >= DataCount) return;
         if (DataCount <= 0) return;
 
-        names = ArrayHelper.Remove(p_idx, names);
-        soundClips = ArrayHelper.Remove(p_idx, soundClips);
-
-        if (DataCount <= 0)
-        {
-            names = null;
-            soundClips = null;
-        }
+        database = ArrayHelper.Remove(p_idx, database);
+        if (DataCount <= 0) database = null;
     }
 
     public override void ClearData()
     {
-        foreach (SoundClip t_clip in soundClips) t_clip.ReleaseClip();
+        foreach (SoundClip t_clip in database) t_clip.ReleaseClip();
 
-        names = null;
-        soundClips = null;
+        database = null;
     }
 
     public override void CopyData(int p_idx)
     {
         if (p_idx < 0 || p_idx >= DataCount) return;
 
-        names = ArrayHelper.Add(names[p_idx], names);
-        soundClips = ArrayHelper.Add(soundClips[p_idx], soundClips);
+        database = ArrayHelper.Add(database[p_idx], database);
     }
 
     #endregion Override Methods
