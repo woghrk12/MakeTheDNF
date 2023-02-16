71128a766e2135d7695ee98fd483f07190d71288
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-02-16 21:50:53 +0900

    데이터 관리 방식을 XML에서 ScriptableObject로 변경

--- Assets/2.Script/GameData/BaseData.cs
+++ Assets/2.Script/GameData/BaseData.cs
@@ -6,42 +6,21 @@ public abstract class BaseData<T> : ScriptableObject
 {
     #region Variables
 
-    public string[] names = null;
-    protected string xmlFilePath = string.Empty;
+    protected T[] database = null;
 
     #endregion Variables
 
     #region Properties
 
-    public int DataCount { get => names == null ? 0 : names.Length; }
+    public int DataCount { get => database == null ? 0 : database.Length; }
 
     #endregion Properties
 
-    #region Helper Methods
-
-    public string[] GetNameList(bool p_isShowID, string p_filterWord = "")
-    {
-        string[] t_retList = new string[0];
-        if (names == null) return t_retList;
-
-        t_retList = new string[names.Length];
-
-        for (int i = 0; i < names.Length; i++)
-        {
-            if (p_filterWord != "" && !names[i].ToLower().Contains(p_filterWord.ToLower())) continue;
-            t_retList[i] = p_isShowID ? i.ToString() + " : " + names[i] : names[i];
-        }
-
-        return t_retList;
-    }
-
-    #endregion Helper Methods
-
     #region Abstract Methods
 
-    public abstract void LoadData();
-    public abstract void SaveData();
-    public abstract T GetCopyClip(int p_idx);
+    public abstract string[] GetNameList(bool p_isShowID, string p_filterWord = "");
+    public abstract T GetClip(int p_idx);
+
     #endregion Abstract Methods
 
     #region Virtual Methods
