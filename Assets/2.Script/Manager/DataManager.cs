9f193a16de383704d17f059d5cedde6b7c142778
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-02-16 23:40:52 +0900

    데이터 접근 로직 수정

--- Assets/2.Script/Manager/DataManager.cs
+++ Assets/2.Script/Manager/DataManager.cs
@@ -6,19 +6,121 @@ public class DataManager : SingletonMonobehaviour<DataManager>
 {
     #region Variables
 
-    [SerializeField] private InputData inputData = null;
-    [SerializeField] private SoundData soundData = null;
-    [SerializeField] private EffectData effectData = null;
-    [SerializeField] private SkillData skillData = null;
+    private static InputData inputData = null;
+    private static SoundData soundData = null;
+    private static EffectData effectData = null;
+    private static SkillData skillData = null;
 
     #endregion Variables
 
     #region Properties
 
-    public InputData InputData => inputData;
-    public SoundData SoundData => soundData;
-    public EffectData EffectData => effectData;
-    public SkillData SkillData => skillData;
+    public static InputData InputData
+    {
+        get
+        {
+            if (ReferenceEquals(inputData, null))
+            {
+                inputData = Resources.Load("Data/Database/InputData") as InputData;
+                if (ReferenceEquals(InputData, null))
+                {
+                    inputData = ScriptableObject.CreateInstance<InputData>();
+                }
+            }
+
+            return inputData;
+        }
+    }
+
+    public static SoundData SoundData
+    {
+        get
+        {
+            if (ReferenceEquals(soundData, null))
+            {
+                soundData = Resources.Load("Data/Database/SoundData") as SoundData;
+                if (ReferenceEquals(soundData, null))
+                {
+                    soundData = ScriptableObject.CreateInstance<SoundData>();
+                }
+            }
+
+            return soundData;
+        }
+    }
+
+    public static EffectData EffectData
+    {
+        get
+        {
+            if (ReferenceEquals(effectData, null))
+            {
+                effectData = Resources.Load("Data/Database/EffectData") as EffectData;
+                if (ReferenceEquals(effectData, null))
+                {
+                    effectData = ScriptableObject.CreateInstance<EffectData>();
+                }
+            }
+
+            return effectData;
+        }
+    }
+
+    public static SkillData SkillData
+    {
+        get
+        {
+            if (ReferenceEquals(skillData, null))
+            {
+                skillData = Resources.Load("Data/Database/SkillData") as SkillData;
+                if (ReferenceEquals(skillData, null))
+                {
+                    skillData = ScriptableObject.CreateInstance<SkillData>();
+                }
+            }
+
+            return skillData;
+        }
+    }
 
     #endregion Properties
+
+    private void Start()
+    {
+        if (ReferenceEquals(inputData, null))
+        {
+            inputData = Resources.Load("Data/Database/InputData") as InputData;
+            if (ReferenceEquals(InputData, null))
+            {
+                inputData = ScriptableObject.CreateInstance<InputData>();
+            }
+        }
+
+        if (ReferenceEquals(soundData, null))
+        {
+            soundData = Resources.Load("Data/Database/SoundData") as SoundData;
+            if (ReferenceEquals(soundData, null))
+            {
+                soundData = ScriptableObject.CreateInstance<SoundData>();
+            }
+        }
+
+        if (ReferenceEquals(effectData, null))
+        {
+            effectData = Resources.Load("Data/Database/EffectData") as EffectData;
+            if (ReferenceEquals(effectData, null))
+            {
+                effectData = ScriptableObject.CreateInstance<EffectData>();
+            }
+        }
+
+        if (ReferenceEquals(skillData, null))
+        {
+            skillData = Resources.Load("Data/Database/SkillData") as SkillData;
+            if (ReferenceEquals(skillData, null))
+            {
+                skillData = ScriptableObject.CreateInstance<SkillData>();
+            }
+        }
+    }
 }
