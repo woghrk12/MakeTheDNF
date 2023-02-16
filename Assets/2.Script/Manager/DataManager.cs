b9063f9d24c93f812a39c1743aeeac205d19dcf4
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2023-02-16 23:33:30 +0900

    데이터 접근 로직 수정

--- Assets/2.Script/Manager/DataManager.cs
+++ Assets/2.Script/Manager/DataManager.cs
@@ -6,91 +6,19 @@ public class DataManager : SingletonMonobehaviour<DataManager>
 {
     #region Variables
 
-    private static InputData inputData = null;
-    private static SoundData soundData = null;
-    private static EffectData effectData = null;
-    private static SkillData skillData = null;
+    [SerializeField] private InputData inputData = null;
+    [SerializeField] private SoundData soundData = null;
+    [SerializeField] private EffectData effectData = null;
+    [SerializeField] private SkillData skillData = null;
 
     #endregion Variables
 
     #region Properties
 
-    public static InputData InputData
-    {
-        get
-        {
-            if (ReferenceEquals(inputData, null))
-            {
-                inputData = ScriptableObject.CreateInstance<InputData>();
-                inputData.LoadData();
-            }
-
-            return inputData;
-        }
-    }
-
-    public static SoundData SoundData
-    {
-        get
-        {
-            if (ReferenceEquals(soundData, null))
-            {
-                soundData = ScriptableObject.CreateInstance<SoundData>();
-                soundData.LoadData();
-            }
-
-            return soundData;
-        }
-    }
-
-    public static EffectData EffectData
-    {
-        get
-        {
-            if (ReferenceEquals(effectData, null))
-            {
-                effectData = ScriptableObject.CreateInstance<EffectData>();
-                effectData.LoadData();
-            }
-
-            return effectData;
-        }
-    }
-
-    public static SkillData SkillData
-    {
-        get
-        {
-            if (ReferenceEquals(skillData, null))
-            {
-                skillData = ScriptableObject.CreateInstance<SkillData>();
-                skillData.LoadData();
-            }
-
-            return skillData;
-        }
-    }
+    public InputData InputData => inputData;
+    public SoundData SoundData => soundData;
+    public EffectData EffectData => effectData;
+    public SkillData SkillData => skillData;
 
     #endregion Properties
-
-    private void Start()
-    {
-        if (ReferenceEquals(soundData, null))
-        {
-            soundData = ScriptableObject.CreateInstance<SoundData>();
-            soundData.LoadData();
-        }
-
-        if (ReferenceEquals(effectData, null))
-        {
-            effectData = ScriptableObject.CreateInstance<EffectData>();
-            effectData.LoadData();
-        }
-
-        if (ReferenceEquals(skillData, null))
-        {
-            skillData = ScriptableObject.CreateInstance<SkillData>();
-            skillData.LoadData();
-        }
-    }
 }
